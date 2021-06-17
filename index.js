6a75c874b7359571d2f02e4c32882e493034f8e3
Pauline122112 <pauline.design007@gmail.com>
2021-06-17 16:04:26 -0500

    completed task 3

--- index.js
+++ index.js
@@ -132,6 +132,10 @@ class Lambdasian {
   constructor(attributes){
     this.name = attributes.name;
     this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
 
